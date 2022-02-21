ebb3ebd461efbb9806c52143d5b06e72f6d8359a
Wayne Schmidt <wschmidt@sumologic.com>
2022-02-21 17:19:25 +0900

    refactored

--- bin/create_operator_list.py
+++ bin/create_operator_list.py
@@ -3,6 +3,11 @@
 This is a quick script to process the sitemap on help.sumologic.com
 """
 
+import os
+import time
+import datetime
+import re
+import sys
 import xmltodict
 import requests
 from bs4 import BeautifulSoup
@@ -12,19 +17,7 @@ SITE_MAP = "https://help.sumologic.com/sitemap.xml"
 my_xml = requests.get(SITE_MAP).text
 my_dict = xmltodict.parse(my_xml)
 
-def get_meta_char(url_base):
-    """
-    This function parses a given page for metadata strings
-    """
-    url_target = url_base + "#rules"
-    url_contents = requests.get(url_target).text
-    soup = BeautifulSoup(url_contents, features="lxml")
-    selector = 'ul strong'
-    quotes = soup.select(selector)
-    varlist = list()
-    for quote in quotes:
-        varlist.append(quote.text)
-    return varlist
+operators = dict()
 
 for url in my_dict['urlset']['url']:
     location = url['loc']
@@ -32,11 +25,8 @@ for url in my_dict['urlset']['url']:
     frequency = url['changefreq']
     verb = location.rsplit('/', 1)[-1]
     if 'Operators' in location:
-        variables = get_meta_char(location)
-        metadata = list()
-        if len(variables) == 0:
-            metadata.append('none')
-        else:
-            for variable in variables:
-                if '_' in variable:
-                    print('{:16} {:30} {}'.format(verb, variable, location))
+        if ( '_' not in verb and '-' not in verb ):
+            operators[verb] = location
+
+for mykey, myvalue in operators.items():
+    print('{},{}'.format(mykey, myvalue))
